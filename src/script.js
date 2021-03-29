7cf5b47570076b61ebac1c20271ce06c549ea05c
Aaron Ojala <aaronojala@yahoo.com>
2021-03-29 19:47:11 +0300

    this should fix it

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 exports.dayOfTheWeek = (date = new Date()) => {
-    const days = ['Sunday', 'Mnday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
     return days[date.getDay()];
 };
